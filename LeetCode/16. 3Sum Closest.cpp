349981eb862b98b2e1a8b71505f412bc335b971d
tlili Firas <firastlili34@gmail.com>
2026-06-28 12:17:43 +0100

    Leetcode Daily 1864

---
+++ LeetCode/16. 3Sum Closest.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        if (nums.size()==3){
+            return(nums[0]+nums[1]+nums[2]);
+        }else{
+            
+        }
+    }
+};
